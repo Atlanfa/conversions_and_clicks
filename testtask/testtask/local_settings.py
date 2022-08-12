ccff833a96111d5b2d44d45c132df11df09a31f8
Atlanfa <iliatalgar@gmail.com>
2022-08-12 15:01:33 +0300

    deploy on localserver

---
+++ testtask/testtask/local_settings.py
@@ -0,0 +1,26 @@
+import os
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-@z)_0ldfvk!8ozt)4yb=svjq099v6k)ajondy3=g38x0vc760v'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
